81c7734635b1b816777db788a6aec86f57e52e5e
ChandaniMankani156 <82370248+ChandaniMankani156@users.noreply.github.com>
2021-06-25 16:05:21 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,27 @@
+class Iron {
+  constructor(x,y){
+ var options = {
+     'restitution' : 0.8,
+     'friction' : 3,
+     'density' : 30
+     }
+     this.body=Bodies.rectangle(x,y,40,40,options);
+     this.width = 20
+     this.height = 20
+     World.add(world,this.body)
+  }
+  display(){
+ var pos = this.body.position
+ var angle = this.body.angle
+ 
+ push();
+ translate(pos.x,pos.y);
+ rotate(angle);
+ strokeWeight(3);
+ stroke('orange')
+ fill('pink')
+ rectMode(CENTER)
+ rect(0, 0, this.width, this.height)
+ pop();
+  }
+}
